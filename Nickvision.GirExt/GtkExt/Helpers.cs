b0ec03248347717edcaf955806d0de8963a6a06d
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-07-25 01:21:23 +0300

    Add extensions for Gtk.ColorButton

--- Nickvision.GirExt/GtkExt/Helpers.cs
+++ Nickvision.GirExt/GtkExt/Helpers.cs
@@ -17,12 +17,34 @@ public unsafe static partial class GtkExt
     }
 
     [LibraryImport("gtk")]
+    private static partial void gtk_color_dialog_button_set_rgba(nint button, ref GdkExt.RGBA rgba);
+    [LibraryImport("gtk")]
     private static partial GLibList* gtk_list_box_get_selected_rows(nint box);
     [LibraryImport("gtk")]
     private static partial int gtk_list_box_row_get_index(nint row);
-    [LibraryImport("gtk")]
-    private static partial void gtk_color_dialog_button_set_rgba(nint button, ref GdkExt.RGBA rgba);
-    
+
+    /// <summary>
+    /// Helper extension method for Gtk.ColorButton to get color as GdkExt.RGBA
+    /// </summary>
+    /// <param name="button">Color button</param>
+    /// <returns>Color as GdkExt.RGBA</returns>
+    public static GdkExt.RGBA GetExtRgba(this Gtk.ColorDialogButton button)
+    {
+        Gdk.RGBA gdkColor = button.GetRgba();
+        return Marshal.PtrToStructure<GdkExt.RGBA>(gdkColor.Handle.DangerousGetHandle());
+    }
+
+    /// <summary>
+    /// Helper extension method for Gtk.ColorButton to set color as GdkExt.RGBA
+    /// </summary>
+    /// <param name="button">Color button</param>
+    /// <param name="color">Color as GdkExt.RGBA</param>
+    public static void SetExtRgba(this Gtk.ColorDialogButton button, GdkExt.RGBA color)
+    {
+        Resolver.SetResolver();
+        gtk_color_dialog_button_set_rgba(button.Handle, ref color);
+    }
+
     /// <summary>
     /// Helper extension method for Gtk.ListBox to get indices of selected row
     /// </summary>
@@ -38,17 +60,5 @@ public unsafe static partial class GtkExt
             list.Add(gtk_list_box_row_get_index(ptr->data));
         }
         return list;
-    }
-
-    public static GdkExt.RGBA GetExtRgba(this Gtk.ColorDialogButton button)
-    {
-        Gdk.RGBA gdkColor = button.GetRgba();
-        return Marshal.PtrToStructure<GdkExt.RGBA>(gdkColor.Handle.DangerousGetHandle());
-    }
-
-    public static void SetExtRgba(this Gtk.ColorDialogButton button, GdkExt.RGBA color)
-    {
-        Resolver.SetResolver();
-        gtk_color_dialog_button_set_rgba(button.Handle, ref color);
     }
 }
